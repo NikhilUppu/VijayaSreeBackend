68a2611668526ad3e76b2b1deb95479507d07276
NikhilUppu <nikhiluppu26@gmail.com>
2026-04-03 12:13:48 -0500

    fix: add @Transactional annotation to SaleServiceImpl for transaction management

--- src/main/java/com/vijayasree/pos/service/impl/SaleServiceImpl.java
+++ src/main/java/com/vijayasree/pos/service/impl/SaleServiceImpl.java
@@ -17,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -30,6 +30,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class SaleServiceImpl implements SaleService {
 
     private final SaleRepository saleRepository;
